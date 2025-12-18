72bee4c9643e307c036fd254df8ffeddd03fcf2f
alinaseem123 <alinaseem21102002@gmail.com>
2025-12-19 00:35:48 +0500

    second7

--- src/components/Home/AboutUs.tsx
+++ src/components/Home/AboutUs.tsx
@@ -10,7 +10,7 @@ export function AboutUs() {
   return (
     <div className="min-h-screen bg-white">
       {/* Story Section with Video */}
-      <section ref={ref} className="py-16">
+      <section ref={ref} className="pt-16 pb-8">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
             
