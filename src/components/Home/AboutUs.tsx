3804fa8ec517e8cb6836b7add6442138c2940821
alinaseem123 <alinaseem21102002@gmail.com>
2025-12-19 01:00:43 +0500

    second

--- src/components/Home/AboutUs.tsx
+++ src/components/Home/AboutUs.tsx
@@ -8,7 +8,7 @@ export function AboutUs() {
     threshold: 0.1,
   });
   return (
-    <div className="min-h-screen bg-white">
+    <div className="bg-white">
       {/* Story Section with Video */}
       <section ref={ref} className="py-16">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
